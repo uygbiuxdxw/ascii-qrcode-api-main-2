08e882d888239f9144786177dacf91990151c91c
Tech Cartel <50111332+Xilbit@users.noreply.github.com>
2024-11-19 17:32:11 -0500

    dv

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="ascii-qrcode-api.onrender.com", port=8000, log_level="info",reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info",reload=True)
